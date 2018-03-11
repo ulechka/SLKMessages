dfd46bf350c9ba8238171bda6028b8bd8a0d2c26
Ulechka Khmeleva <ulechka.hmeleva@gmail.com>
2018-03-11 13:46:51 +0700

    add fiels of SLK messages controller

---
+++ SLKMessages/SLKMessages.h
@@ -0,0 +1,31 @@
+//
+//  SLKMessages.h
+//  SLKMessages
+//
+//  Created by Uliana Khmeleva on 11/03/2018.
+//  Copyright © 2018 UP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SLKMessages.
+FOUNDATION_EXPORT double SLKMessagesVersionNumber;
+
+//! Project version string for SLKMessages.
+FOUNDATION_EXPORT const unsigned char SLKMessagesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLKMessages/PublicHeader.h>
+
+#import <SLKMessages/SLKInputAccessoryView.h>
+#import <SLKMessages/SLKTextInput.h>
+#import <SLKMessages/SLKTextInputbar.h>
+#import <SLKMessages/SLKTextView+SLKAdditions.h>
+#import <SLKMessages/SLKTextView.h>
+#import <SLKMessages/SLKTextViewController.h>
+#import <SLKMessages/SLKTypingIndicatorProtocol.h>
+#import <SLKMessages/SLKTypingIndicatorView.h>
+#import <SLKMessages/SLKUIConstants.h>
+#import <SLKMessages/UIResponder+SLKAdditions.h>
+#import <SLKMessages/UIScrollView+SLKAdditions.h>
+#import <SLKMessages/UIView+SLKAdditions.h>
+
